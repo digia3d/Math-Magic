6e41e438ed6053e7a3d702d68fba396a1551e768
Aleksandra Ujvari <ujvari65@gmail.com>
2022-08-01 23:05:16 +0200

    Fix linters errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './Calculator.css';
+import React from 'react';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,42 +1,44 @@
 import React from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
+class Calculator extends React.Component {
   render() {
     return (
-      <div className='calculator-cont'>
-        <div className='result'>0</div>
-        <div className='button'>
-          <button className='btn-opr' type='button'>AC</button>
-          <button className='btn-opr' type='button'>+/-</button>
-          <button className='btn-opr' type='button'>%</button>
-          <button className='btn-opr-right' type='button'>÷</button>
+      <div className="calculator-cont">
+        <div className="result">0</div>
+        <div className="button">
+          <button className="btn-opr" type="button">AC</button>
+          <button className="btn-opr" type="button">+/-</button>
+          <button className="btn-opr" type="button">%</button>
+          <button className="btn-opr-right" type="button">÷</button>
         </div>
-        <div className='button'>
-          <button className='btn-opr' type='button'>7</button>
-          <button className='btn-opr' type='button'>8</button>
-          <button className='btn-opr' type='button'>9</button>
-          <button className='btn-opr-right' type='button'>x</button>
+        <div className="button">
+          <button className="btn-opr" type="button">7</button>
+          <button className="btn-opr" type="button">8</button>
+          <button className="btn-opr" type="button">9</button>
+          <button className="btn-opr-right" type="button">x</button>
         </div>
-        <div className='button'>
-          <button className='btn-opr' type='button'>4</button>
-          <button className='btn-opr' type='button'>5</button>
-          <button className='btn-opr' type='button'>6</button>
-          <button className='btn-opr-right' type='button'>-</button>
+        <div className="button">
+          <button className="btn-opr" type="button">4</button>
+          <button className="btn-opr" type="button">5</button>
+          <button className="btn-opr" type="button">6</button>
+          <button className="btn-opr-right" type="button">-</button>
         </div>
-        <div className='button'>
-          <button className='btn-opr' type='button'>1</button>
-          <button className='btn-opr' type='button'>2</button>
-          <button className='btn-opr' type='button'>3</button>
-          <button className='btn-opr-right' type='button'>+</button>
+        <div className="button">
+          <button className="btn-opr" type="button">1</button>
+          <button className="btn-opr" type="button">2</button>
+          <button className="btn-opr" type="button">3</button>
+          <button className="btn-opr-right" type="button">+</button>
         </div>
-        <div className='button'>
-          <button className='btn-opr-zero' type='button'>0</button>
-          <button className='btn-opr' type='button'>.</button>
-          <button className='btn-opr-right' type='button'>=</button>
+        <div className="button">
+          <button className="btn-opr-zero" type="button">0</button>
+          <button className="btn-opr" type="button">.</button>
+          <button className="btn-opr-right" type="button">=</button>
         </div>
 
       </div>
-    )
+    );
   }
 }
+
+export default Calculator;
