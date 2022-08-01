d135ca137d4db20ffa432a4e325831586d30bdb1
Aleksandra Ujvari <ujvari65@gmail.com>
2022-08-01 22:41:30 +0200

    Add first row of buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div className='calculator-cont'>
+        <div className='result'>0</div>
+        <div className='button'>
+          <button className='btn-opr' type='button'>AC</button>
+          <button className='btn-opr' type='button'>+/-</button>
+          <button className='btn-opr' type='button'>%</button>
+          <button className='btn-opr-right' type='button'>÷</button>
+        </div>
+      </div>
+    )
+  }
+}
