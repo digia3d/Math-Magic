7b3378971818f8ca43b10b17532f110143b5a1e2
Aleksandra Ujvari <ujvari65@gmail.com>
2022-08-01 22:49:02 +0200

    Add rest of the buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,6 +12,30 @@ class App extends React.Component {
           <button className='btn-opr' type='button'>%</button>
           <button className='btn-opr-right' type='button'>÷</button>
         </div>
+        <div className='button'>
+          <button className='btn-opr' type='button'>7</button>
+          <button className='btn-opr' type='button'>8</button>
+          <button className='btn-opr' type='button'>9</button>
+          <button className='btn-opr-right' type='button'>x</button>
+        </div>
+        <div className='button'>
+          <button className='btn-opr' type='button'>4</button>
+          <button className='btn-opr' type='button'>5</button>
+          <button className='btn-opr' type='button'>6</button>
+          <button className='btn-opr-right' type='button'>-</button>
+        </div>
+        <div className='button'>
+          <button className='btn-opr' type='button'>1</button>
+          <button className='btn-opr' type='button'>2</button>
+          <button className='btn-opr' type='button'>3</button>
+          <button className='btn-opr-right' type='button'>+</button>
+        </div>
+        <div className='button'>
+          <button className='btn-opr-zero' type='button'>0</button>
+          <button className='btn-opr' type='button'>.</button>
+          <button className='btn-opr-right' type='button'>=</button>
+        </div>
+
       </div>
     )
   }
