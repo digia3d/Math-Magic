4d628449e7e9afc7cac99346f841ad167ec0d4e2
Aleksandra Ujvari <ujvari65@gmail.com>
2022-08-11 10:41:49 +0200

    Add tests for components

--- src/__tests__/calculate.test.jsx
+++ src/__tests__/calculate.test.jsx
@@ -1,7 +1,23 @@
 import calculate from '../components/logic/calculate';
 
 describe('Test for `calculate`', () => {
-  it('should return the correct value', () => {
-    expect()
+  it('should display the correct value', () => {
+    const testValue = {
+      total: 0,
+      next: null,
+      operation: null,
+    }
+    const result = calculate(testValue, '7');
+    expect(result.next).toEqual('7');
+  })
+
+  it('operations', () => {
+    const testValue = {
+      total: 0,
+      next: 5,
+      operation: 'sum',
+    }
+    const result = calculate(testValue, 'x');
+    expect(result.operation).toEqual('x');
   })
 })
