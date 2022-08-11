13932e39d05102eed522aa74923493f27883c055
Aleksandra Ujvari <ujvari65@gmail.com>
2022-08-11 10:21:14 +0200

    Add tests for components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import './components/App.css';
@@ -10,8 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
